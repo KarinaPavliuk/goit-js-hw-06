085377e70b33948ad575ca30a4deef57641e327c
KarinaPavliuk <kpavliuk8@gmail.com>
2023-06-11 19:59:30 +0300

    update-hw-js

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const list = document.querySelectorAll('.item');
+console.log("Number of categories:", list.length);
+
+const heading = document.querySelectorAll('h2');
+
+// const headingText = Object.values(heading);
+
+// for (const key in heading) {
+//   console.log("Category:", heading[2]);
+// }
+
+// const headingText = heading.textContent;
+console.log(heading);
+console.dir(heading.textContent);
+
+
